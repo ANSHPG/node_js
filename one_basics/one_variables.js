470492d4b5029743cd54b06a4dbed8653ad1ebe5
Anshuman Pattnaik <helloanshu04@gmail.com>
2025-04-20 14:59:43 +0000

    variables and datatypes

---
+++ one_basics/one_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14453
+let accountEmail = "anshu@google.com" // new
+var accountPassword = "1234" // old
+accountCity = "BBSR"
+
+//accountId = 6 - not allowed
+accountEmail = "23@gmail.com"
+accountPassword = "345"
+accountCity = "Delhi"
+
+let accountState 
+
+console.log(accountId);
+console.table([accountEmail, accountCity, accountPassword,accountState])
+
---
+++ one_basics/two_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // new js verson
+
+let name = "anshu" // string = ""
+let age = 20 // number => 2^53
+let isLoggedIn = false // boolean
+
+// bigInt
+// null = standalone = empty
+// symbol => uniqness
+// undefined
+
+console.log (typeof "anshu")
