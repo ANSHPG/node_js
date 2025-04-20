ce5e455d966c8e3fff533d6e43aa6afc6f263f50
Anshuman Pattnaik <helloanshu04@gmail.com>
2025-04-20 15:37:53 +0000

    conversion

---
+++ one_basics/three_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33a"
+num = Number(score)
+
+console.log(typeof score)
+console.log(typeof num)
+console.log(num)
+
+let binary = 19
+let boolean = Boolean(binary)
+
+console.log(boolean)
+
