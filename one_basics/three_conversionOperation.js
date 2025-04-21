9c9a440c55a798cf75372dcf36b2ea00e59a7397
Anshuman Pattnaik <helloanshu04@gmail.com>
2025-04-21 04:29:03 +0000

    comparision

--- one_basics/three_conversionOperation.js
+++ one_basics/three_conversionOperation.js
@@ -1,12 +1,21 @@
 let score = "33a"
 num = Number(score)
 
-console.log(typeof score)
-console.log(typeof num)
-console.log(num)
+// console.log(typeof score)
+// console.log(typeof num)
+// console.log(num)
 
 let binary = 19
 let boolean = Boolean(binary)
 
-console.log(boolean)
+// console.log(boolean)
 
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(2 + 2 + "1")
+
+// readability
+let gameCouter = 100
+gameCouter++
+console.log(gameCouter)
