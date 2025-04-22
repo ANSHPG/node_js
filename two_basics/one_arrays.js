402f851f1fd5e8a5382fc096cd23ade66399b661
Anshuman Pattnaik <helloanshu04@gmail.com>
2025-04-22 10:33:27 +0000

    begin_second_basics

---
+++ two_basics/one_arrays.js
@@ -0,0 +1,21 @@
+const arr = [0,1,2,3,4,5]
+const heroes = ["ironman","captainamerica"]
+
+// array methods 
+
+arr.push(6)
+arr.pop()
+arr.unshift(9)
+arr.shift()
+// console.log(arr.includes(4))
+// console.log(arr.indexOf(9))
+
+const newArr = arr.join()
+// console.log(typeof newArr)
+
+
+console.log(arr.slice(1,3))
+console.log(arr.splice(1,3))
+console.log(arr)
+
+
